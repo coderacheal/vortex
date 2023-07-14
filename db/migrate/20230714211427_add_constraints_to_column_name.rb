263fa6cb385a6af90de2a6b433b5186e2451754c
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-07-14 21:23:14 +0000

    Add schema.rb as migration run

--- db/migrate/20230714211427_add_constraints_to_column_name.rb
+++
@@ -1,6 +0,0 @@
-class AddConstraintsToColumnName < ActiveRecord::Migration[7.0]
-  def change
-    change_column :users, :posts_counter :integer, null: false, default: 0
-    # change_column :users, :posts_counter, "integer USING CAST(posts_counter AS integer)", null: false, default: 0
-  end
-end

2c5d6973208ea70329f3bed6e93ee64a8f4d4e87
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-07-15 10:20:54 +0000

    Add method to Post class and associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+  # The 5 most recent comments for a given post
+
+  after_save :update_posts_count
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  # Updates the post counter for a post
+  private
+
+  def update_posts_count
+    author.increment!(:posts_counter)
+  end
 end

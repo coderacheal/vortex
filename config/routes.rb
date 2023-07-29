5185d2239546e8fef27b9b806788c23b09ac870c
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-07-29 13:38:14 +0000

    Create API in controller, add name space to routes.rb and generate files for post, user, and comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,19 @@ Rails.application.routes.draw do
     end
   end
 
+  #Adding API
+  # namespace :api do
+  #   get 'users', to: 'users#index'
+  # end
+
+  namespace :api, defaults: { format: 'json' } do
+    resources :users, only: [] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index, :create]
+      end
+    end
+  end
+
   root "users#index"
 
 end
